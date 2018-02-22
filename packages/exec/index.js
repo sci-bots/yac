e60deeff9db978fc8b25cfd80ee171b107f205a5
lucaszw <lucas.zeer@gmail.com>
2018-02-22 12:23:44 -0500

    add exec command

---
+++ packages/exec/index.js
@@ -0,0 +1,22 @@
+const os = require('os');
+const {spawnSync} = require('child_process');
+
+let options = {stdio: 'inherit', shell: true};
+
+module.exports = (cwd=undefined, cmd) => {
+  if (cwd == undefined) cwd = process.cwd();
+  options.cwd = cwd;
+
+  if (os.platform() == 'win32') {
+    cmd = `activate ./yac_environment ; ${cmd}`;
+  } else {
+    cmd = `source activate ./yac_environment && ${cmd}`;
+  }
+
+  console.log("Executing: ", cmd);
+  spawnSync(cmd,[], options);
+}
+
+if (require.main === module) {
+  module.exports();
+}
